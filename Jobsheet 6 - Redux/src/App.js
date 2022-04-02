e84c78e096072fa2bb8464449e05b80792b92b12
nailanabilaaaaaa <nailanabilaaaaaa@gmail.com>
2022-04-02 20:58:16 +0700

    Praktikum Redux - Modul 6

--- Jobsheet 6 - Redux/src/App.js
+++ Jobsheet 6 - Redux/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Table from "./containers/Table";
+import CreateToDo from "./containers/CreateTodo";
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <CreateToDo />
+      <Table />
     </div>
   );
 }
--- Jobsheet 6 - Redux/src/index.js
+++ Jobsheet 6 - Redux/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import MainReducer from './reducers/MainReducer';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => 
+f)(createStore)(MainReducer)
+
+ReactDOM.render(<Provider store={store}>
+  <App />
+  </Provider>
+  , document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
