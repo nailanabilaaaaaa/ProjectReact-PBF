88dfa77eade2243792b335343af2347451acf7d6
nailanabilaaaaaa <nailanabilaaaaaa@gmail.com>
2022-04-24 23:19:14 +0700

    Praktikum Firebase Auth - Modul 10

---
+++ Jobsheet 10 - Firebase Auth/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDAorX_NumuattWEKrasGUjDDAhMPzLyHA",
+    authDomain: "jobsheet-10---firebase-auth.firebaseapp.com",
+    projectId: "jobsheet-10---firebase-auth",
+    storageBucket: "jobsheet-10---firebase-auth.appspot.com",
+    messagingSenderId: "131299048730",
+    appId: "1:131299048730:web:093a3d88be791f3f7c80a3",
+    measurementId: "G-FZ06YF717L"
+};
